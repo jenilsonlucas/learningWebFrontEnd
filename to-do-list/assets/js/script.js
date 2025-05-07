6470132dda308b5d754973eea50b0eca1ac7e5e8
jenilsonlucas <jenilsonllucas@gmail.com>
2025-05-07 10:40:32 +0100

    refactor(form-create-task): refantorando o js do formulario de ciração de tarefas

--- to-do-list/assets/js/script.js
+++ to-do-list/assets/js/script.js
@@ -7,6 +7,10 @@ const menuIcon = document.querySelector('.menu-icon');
 const navbarAside = document.querySelector('aside');
 const content = document.querySelector('.content');
 const asideBtn = document.querySelector('.aside-btn-task');
+const caretDropdown = document.querySelector('.caret');
+const menuDropdown = document.querySelector('.dropdown .menu');
+const selectDropdow = document.querySelector('.dropdown .select');
+const optionsDropdown = document.querySelectorAll('.dropdown .menu li');
 
 //elementos do formulário de criação de tarefas
 const formCreate = document.querySelector('.form-create-task');
@@ -16,6 +20,10 @@ const textAreaBox = document.querySelector('.form-create-task .textarea-box div'
 const textAreaTask = document.querySelector('.form-create-task .textarea-box div textarea');
 const selectBox = document.querySelector('.form-create-task .select-box select')
 const iconClose = document.querySelector('.icon-close');
+const btnSubmitTask = document.querySelector('.form-create-task .form-create .btn-task .btn');
+const leavingFormTask = document.querySelector('.leaving-container');
+const btnLeavingKeep = document.querySelector('.btn-leaving .keep');
+const btnLeavingLeave = document.querySelector('.btn-leaving .leave');
 
 //elementos do formulário de criação de cartegory
 const formCreateCategory = document.querySelector('.form-create-category')
@@ -28,13 +36,6 @@ const messageCategory = document.querySelector('.message-category');
 //colocando a visibilidade do formulário de tarefas
 asideBtn.addEventListener('click', () => {
     formCreate.classList.add('active-form');
-});
-
-//tirando a visibidade do formulário de criação de tarefas
-iconClose.addEventListener('click', () => {
-    inputTask.value = '';
-    textAreaTask.value = '';
-    formCreate.classList.remove('active-form');
 });
 
 //colocando efeito nos elementos do formulário de criação de tarefa
@@ -62,7 +63,42 @@ selectBox.addEventListener('blur', () => {
     selectBox.classList.remove('active');
 });
 
+inputTask.addEventListener('input', ()=> {
+    if(inputTask.value.length > 0)
+        btnSubmitTask.classList.add('active');
+    else
+        btnSubmitTask.classList.remove('active');
+});
 
+//tirando a visibidade do formulário de criação de tarefas
+iconClose.addEventListener('click', () => {
+    if(inputTask.value.length > 0)
+    leavingFormTask.classList.add('active');
+    else 
+    formCreate.classList.remove('active-form');
+});
+
+btnLeavingKeep.addEventListener('click', () => {
+    leavingFormTask.classList.remove('active');
+});
+
+btnLeavingLeave.addEventListener('click', () => {
+    leavingFormTask.classList.remove('active');
+    inputTask.value = '';
+    textAreaTask.value = '';
+    formCreate.classList.remove('active-form');
+})
+
+selectDropdow.addEventListener('click', () => {
+    caretDropdown.classList.toggle('rotate');
+    menuDropdown.classList.toggle('active');
+})
+
+optionsDropdown.forEach( option => {
+    option.addEventListener('click', () => {
+        option.classList.toggle('check');
+    });
+});
 /*
  * quando icon do menu lateral é clicado:
  * Alterna a visibilidade do menu lateral com a class active-aside
@@ -89,6 +125,7 @@ btnCancelCategory.addEventListener('click', () => {
     inputCategory.value = '';
     messageCategory.classList.remove('active');
     formCreateCategory.classList.remove('active');
+    btnSubmitCategory.classList.remove('active');
 });
 
 inputCategory.addEventListener('focus', () => {
@@ -101,7 +138,6 @@ inputCategory.addEventListener('blur', () => {
 
 inputCategory.addEventListener('input', () => {
 
-
     if(inputCategory.value.length > 0){
         messageCategory.classList.remove('active');
         btnSubmitCategory.classList.add('active');
@@ -110,3 +146,5 @@ inputCategory.addEventListener('input', () => {
         btnSubmitCategory.classList.remove('active');      
     }  
 });
+
+
